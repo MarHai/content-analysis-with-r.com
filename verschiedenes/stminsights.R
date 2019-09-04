c09ea7a2bb59eb3c1cc569744754cabfce3e42f7
Mario Haim <mario@haim.it>
2019-09-04 10:00:56 +0200

    leftovers removed

--- verschiedenes/stminsights.R
+++
@@ -1,6 +0,0 @@
-# Installation und Laden der Bibliotheken
-if(!require("quanteda")) install.packages("quanteda")
-if(!require("tidyverse")) install.packages("tidyverse")
-if(!require("stminsights")) install.packages("stminsights")
-
-run_stminsights()
